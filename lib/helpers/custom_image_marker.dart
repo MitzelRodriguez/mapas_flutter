6050302dcb6cf24a54d00ad0cb8a2f985ff26876
Sennit <sennitoficial@gmail.com>
2021-01-18 14:32:27 -0600

    Ultimos cambios

--- lib/helpers/custom_image_marker.dart
+++ lib/helpers/custom_image_marker.dart
@@ -15,19 +15,19 @@ Future<BitmapDescriptor> getAssetImageMarker() async {
 Future<BitmapDescriptor> getNetworkImageMarker() async {
   //Peticion http para traer el recurso
   final resp = await Dio().get(
-      'https://cdn4.iconfinder.com/data/icons/small-n-flat/24/map-marker-512.png', 
-      options: Options(responseType: ResponseType.bytes);
-    );
+      'https://cdn4.iconfinder.com/data/icons/small-n-flat/24/map-marker-512.png',
+      options: Options(responseType: ResponseType.bytes));
 
   final bytes = resp.data;
 
-  //Opcional 
-  final imageCodec = await ui.instantiateImageCodec(bytes, targetHeight: 150, targetWidth: 150);
+  //Opcional
+  final imageCodec = await ui.instantiateImageCodec(bytes,
+      targetHeight: 150, targetWidth: 150);
   final frame = await imageCodec.getNextFrame();
 
   //extraer los bites
   final data = await frame.image.toByteData(format: ui.ImageByteFormat.png);
-  
 
-  return await BitmapDescriptor.fromBytes(data.buffer.asUint8List());// cambiar el tamanno
+  return await BitmapDescriptor.fromBytes(
+      data.buffer.asUint8List()); // cambiar el tamanno
 }
